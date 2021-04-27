6e84e375f9cec7b889f98d453d526deafd5b0b15
Matthew Gregg <matt.t.gregg@gmail.com>
2021-04-27 22:32:19 +0100

    Remove unncecessary 'query' function.

--- temperature-tools/src/lib.rs
+++ temperature-tools/src/lib.rs
@@ -87,32 +87,9 @@ async fn home_client<'a>() -> Result<tokio_postgres::Client, TemperatureError> {
     Ok(client)
 }
 
-async fn query(query: &str) -> Result<Vec<tokio_postgres::Row>, TemperatureError> {
-    dotenv::dotenv().ok();
-    let usr = dotenv::var("HOME_USER");
-
-    let default_conn_string = format!(
-        "host='localhost' dbname='home' user='{}'",
-        usr.unwrap_or("<<HOME_USER>>".to_string())
-    );
-    let conn_string = dotenv::var("HOME_CONN").unwrap_or(default_conn_string);
-
-    // Connect to the database.
-    let (client, connection) = tokio_postgres::connect(&conn_string, NoTls).await?;
-
-    // The connection object performs the actual communication with the database,
-    // so spawn it off to run on its own.
-    tokio::spawn(async move {
-        if let Err(e) = connection.await {
-            eprintln!("connection error: {}", e);
-        }
-    });
-
-    client.query(query, &[]).await.map_err(|err| TemperatureError::DatabaseError(err))
-}
-
 pub async fn last_temp() -> Result<TimedTemp, TemperatureError> {
-    let rows = query("SELECT * FROM temperatures ORDER BY time DESC LIMIT 1").await?;
+    let client = home_client().await?;
+    let rows = client.query("SELECT * FROM temperatures ORDER BY time DESC LIMIT 1", &[]).await?;
 
     if let Some(latest) = rows.first() {
         let temp: Decimal = latest.get(1);
@@ -128,11 +105,11 @@ pub async fn last_temp() -> Result<TimedTemp, TemperatureError> {
     } else {
         Err(TemperatureError::IOError("no temperatures found".to_string()))
     }
-
 }
 
 pub async fn all_temps() -> Result<Vec<TimedTemp>, TemperatureError> {
-    let rows = query("SELECT * FROM temperatures").await?;
+    let client = home_client().await?;
+    let rows = client.query("SELECT * FROM temperatures", &[]).await?;
 
     let mut temps = vec![];
 
